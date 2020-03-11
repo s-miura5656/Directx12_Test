ad9533e074df96236b82d8e2a58b3fdc6515ecc4
souta_miura <miura042910@gmail.com>
2020-03-11 16:58:33 +0900

    4.6.3まで完了

--- DirectX12_Test/main.cpp
+++ DirectX12_Test/main.cpp
@@ -1,10 +1,11 @@
 //ウィンドウ表示＆DirectX初期化
-#include<Windows.h>
-#include<tchar.h>
-#include<d3d12.h>
-#include<dxgi1_6.h>
-#include<vector>
-#include<DirectXMath.h>
+#include <Windows.h>
+#include <tchar.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+#include <vector>
+#include <d3dcompiler.h>
 
 #ifdef _DEBUG
 #include<iostream>
@@ -13,6 +14,7 @@
 
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"d3dcompiler.lib")
 
 using namespace DirectX;
 
@@ -230,8 +232,65 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	vbView.SizeInBytes = sizeof(vertices);
 	vbView.StrideInBytes = sizeof(vertices[0]);
 
+	ID3DBlob* _vsBlob = nullptr;
+	ID3DBlob* _psBlob = nullptr;
+
+	ID3DBlob* errorBlob = nullptr;
+
+	result = D3DCompileFromFile(L"BasicVertexShader.hlsl",
+								nullptr,
+								D3D_COMPILE_STANDARD_FILE_INCLUDE,
+								"BasicVS", "vs_5_0",
+								D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
+								0, &_vsBlob, &errorBlob);
+
+	if (FAILED(result))
+	{
+		if (result == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
+		{
+			::OutputDebugStringA(" ファイルが見当たりません ");
+			return 0;
+		}
+		else
+		{
+			std::string errstr;
+			errstr.resize(errorBlob->GetBufferSize());
+			std::copy_n((char*)errorBlob->GetBufferPointer(),
+							   errorBlob->GetBufferSize(),
+							   errstr.begin());
+			errstr += "\n";
+			::OutputDebugStringA(errstr.c_str());
+		}
+	}
+
+	result = D3DCompileFromFile(L"BasicPixelShader.hlsl",
+								nullptr,
+								D3D_COMPILE_STANDARD_FILE_INCLUDE,
+								"BasicPS", "ps_5_0",
+								D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
+								0, &_psBlob, &errorBlob);
+
+	if (FAILED(result))
+	{
+		if (result == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
+		{
+			::OutputDebugStringA(" ファイルが見当たりません ");
+			return 0;
+		}
+		else
+		{
+			std::string errstr;
+			errstr.resize(errorBlob->GetBufferSize());
+			std::copy_n((char*)errorBlob->GetBufferPointer(),
+							   errorBlob->GetBufferSize(),
+							   errstr.begin());
+			errstr += "\n";
+			::OutputDebugStringA(errstr.c_str());
+		}
+	}
 
 	MSG msg = {};
+
 	while (true) {
 
 		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
