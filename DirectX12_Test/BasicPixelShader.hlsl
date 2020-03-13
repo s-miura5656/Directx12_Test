4e8fd1113855c73d32d7b64343443641da261647
souta_miura <miura042910@gmail.com>
2020-03-13 12:53:47 +0900

    5.10まで完了 ( gitエラー復旧が大変だった( ;∀;) )

--- DirectX12_Test/BasicPixelShader.hlsl
+++ DirectX12_Test/BasicPixelShader.hlsl
@@ -1,6 +1,9 @@
 #include "BasicShaderHeader.hlsli"
 
+Texture2D<float4> tex : register(t0);
+SamplerState smp : register(s0);
+
 float4 BasicPS( Output input) : SV_TARGET
 {
-	return float4(input.uv, 1.0f, 1.0f);
+    return float4(tex.Sample(smp, input.uv));
 }
