03d2cb106bd866c66cc8fcaa53c37aa491431298
souta_miura <miura042910@gmail.com>
2020-03-14 17:55:26 +0900

    6.2.2まで完了 ( マップによる定数のコピー )

--- DirectX12_Test/main.cpp
+++ DirectX12_Test/main.cpp
@@ -513,7 +513,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		  texDescHeap->GetCPUDescriptorHandleForHeapStart()
 	);
 
+	XMMATRIX matrix = XMMatrixIdentity();
 
+	ID3D12Resource* constBuff = nullptr;
+
+	result = _dev->CreateCommittedResource(
+				   &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+				   D3D12_HEAP_FLAG_NONE,
+				   &CD3DX12_RESOURCE_DESC::Buffer((sizeof(matrix) + 0xff) & ~0xff),
+				   D3D12_RESOURCE_STATE_GENERIC_READ,
+				   nullptr,
+				   IID_PPV_ARGS(&constBuff));
+
+	XMMATRIX* mapMatrix;
+
+	result = constBuff->Map(0, nullptr, (void**)&mapMatrix);
+
+	*mapMatrix = matrix;
 
 	MSG msg = {};
 
