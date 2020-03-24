bca3688195ad9ef0c7e9f754eed20a199b6c5a7a
souta_miura <miura042910@gmail.com>
2020-03-24 16:16:10 +0900

    8.9.5まで完了 ( デフォルトグラデーションテクスチャの作成 )

--- DirectX12_Test/main.cpp
+++ DirectX12_Test/main.cpp
@@ -290,6 +290,66 @@ ID3D12Resource* CreateBlackTexture()
 	return blackBuff;
 }
 
+ID3D12Resource* CreateGrayGradationTexture() 
+{
+	D3D12_RESOURCE_DESC resDesc = {};
+	resDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	resDesc.Width = 4;
+	resDesc.Height = 256;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.SampleDesc.Quality = 0;
+	resDesc.MipLevels = 1;
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;//レイアウトについては決定しない
+	resDesc.Flags = D3D12_RESOURCE_FLAG_NONE;//とくにフラグなし
+
+
+	D3D12_HEAP_PROPERTIES texHeapProp = {};
+	texHeapProp.Type = D3D12_HEAP_TYPE_CUSTOM;//特殊な設定なのでdefaultでもuploadでもなく
+	texHeapProp.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;//ライトバックで
+	texHeapProp.MemoryPoolPreference = D3D12_MEMORY_POOL_L0;//転送がL0つまりCPU側から直で
+	texHeapProp.CreationNodeMask = 0;//単一アダプタのため0
+	texHeapProp.VisibleNodeMask = 0;//単一アダプタのため0
+
+	ID3D12Resource* gradBuff = nullptr;
+	auto result = _dev->CreateCommittedResource(
+						&texHeapProp,
+						D3D12_HEAP_FLAG_NONE,//特に指定なし
+						&resDesc,
+						D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+						nullptr,
+						IID_PPV_ARGS(&gradBuff)
+	);
+
+	if (FAILED(result)) {
+		return nullptr;
+	}
+
+	// 上が白くて下が黒いテクスチャデータを作成
+	std::vector<unsigned int> data(4 * 256);
+	
+	auto it = data.begin();
+	
+	unsigned int c = 0xff;
+
+	for (; it != data.end(); it += 4)
+	{
+		auto col = (c << 0xff) | (c << 16) | (c << 8) | c;
+		std::fill(it, it + 4, col);
+		--c;
+	}
+
+	result = gradBuff->WriteToSubresource(
+					   0,
+					   nullptr,
+					   data.data(),
+					   4 * sizeof(unsigned int),
+					   sizeof(unsigned int) * data.size());
+
+	return gradBuff;
+}
+
 
 // テクスチャのパスをセパレーター文字で分離する
 // @param path 対象のパス文字列
@@ -358,8 +418,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// デバッグレイヤーをオンに
 	EnableDebugLayer();
 #endif
-
-	
 
 	// DirectX12まわり初期化
 	// フィーチャレベル列挙
@@ -668,6 +726,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	std::vector<ID3D12Resource*> textureResources(materialNum);
 	std::vector<ID3D12Resource*> sphResources(materialNum);
 	std::vector<ID3D12Resource*> spaResources(materialNum);
+	std::vector<ID3D12Resource*> toonResources(materialNum);
 	std::vector<PMDMaterial> pmdMaterials(materialNum);
 	
 	fread(pmdMaterials.data(), pmdMaterials.size() * sizeof(PMDMaterial), 1, fp);
@@ -686,6 +745,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	for (int i = 0; i < pmdMaterials.size(); ++i)
 	{
+		// トゥーンリソースの読み込み
+		std::string toonFilePath = "Content/Toon/";
+
+		char toonFileName[16];
+
+		sprintf(toonFileName, "toon_%02d.bmp", pmdMaterials[i].toonIdx + 1);
+
+		toonFilePath += toonFileName;
+
+		toonResources[i] = LoadTextureFromFile(toonFilePath);
+
 		std::string texFileName = pmdMaterials[i].texFilePath;
 		std::string sphFileName = "";
 		std::string spaFileName = "";
@@ -858,6 +928,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	auto whiteTex = CreateWhiteTexture();
 	auto blackTex = CreateBlackTexture();
+	auto gradTex = CreateGrayGradationTexture();
 
 	for (int i = 0; i < materialNum; ++i)
 	{
